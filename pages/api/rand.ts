2a435653e6ad30e4d9f82141cf62afb2f029b079
Surya <git@sdan.io>
2022-11-25 22:04:31 -0500

    done

--- pages/api/rand.ts
+++ pages/api/rand.ts
@@ -1,6 +1,5 @@
 import { Alchemy, Network } from "alchemy-sdk";
 import { NextApiRequest, NextApiResponse } from "next";
-import sharp from "sharp";
 
 const config = {
     apiKey: "4UCYqRJivQQ9chYdyxgUeTOiHQiRYq8O",
@@ -12,17 +11,19 @@ const config = {
 export default async function handler(req: any, res: any) {
 
 
-   // Contract address
-  const bayc = "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D";
-  const wizards = "0xc23b12eba3af92dc3ec94744c0c260cad0eed0e5";
-  const address = "0xa7d8d9ef8d8ce8992df33d8b8cf4aebabd5bd270";
+  // Contract address
+   const addressPool = ['0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D','0x059edd72cd353df5106d2b9cc5ab83a52287ac3a','0xa7d8d9ef8d8ce8992df33d8b8cf4aebabd5bd270','0xd10e3dee203579fcee90ed7d0bdd8086f7e53beb','0x28f2d3805652fb5d359486dffb7d08320d403240','0xbdde08bd57e5c9fd563ee7ac61618cb2ecdc0ce0','0x13aae6f9599880edbb7d144bb13f1212cee99533','0x0a1bbd57033f57e7b6743621b79fcb9eb2ce3676','0xa319c382a702682129fcbf55d514e61a16f97f9c','0x62e37f664b5945629b6549a87f8e10ed0b6d923b']
+
+  // Pick random contract from pool
+  const randomContract = addressPool[Math.floor(Math.random() * addressPool.length)];
+
 
   // Flag to omit metadata
   const omitMetadata = false;
 
   // Get all NFTs
-  const response = await alchemy.nft.getNftsForContract(wizards, {
-    pageSize: 10,
+  const response = await alchemy.nft.getNftsForContract(randomContract, {
+    pageSize: 50,
     omitMetadata: omitMetadata,
   });
 
@@ -36,21 +37,7 @@ export default async function handler(req: any, res: any) {
 
 
 // detect what file type url is
-
-if(imgdata.includes("data:image")){
-  // Retrun svg image as base64
-  sharp(Buffer.from(imgdata.replace(/^data:image\/\w+;base64,/, ""), 'base64'))
-  .png()
-  .toBuffer()
-  .then(data => {
-    res.status(200).json({ img: data.toString('base64') })
-  })
-  .catch(err => {
-    console.log(err);
-  });
-
-
-} else if(imgdata.includes("ipfs://")){
+if(imgdata.includes("ipfs://")){
   const ipfsUrl = imgdata.replace("ipfs://", "https://ipfs.io/ipfs/");
   const ipfsdata = await fetch(ipfsUrl);
   const arrayBuffer = await ipfsdata.arrayBuffer();
